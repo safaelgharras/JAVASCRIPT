ef124418b24292067b648463679d2a327693bb44
safaelgharras <ehsafaa7@gmail.com>
2025-10-17 16:57:57 +0100

    file

---
+++ Réalisation 4/script1.js
@@ -0,0 +1,39 @@
+
+let input = document.getElementById("inputTache");
+let liste = document.getElementById("liste");
+let ajouterBouton = document.getElementById("ajouter");
+
+ajouterBouton.addEventListener('click', () => {
+    let texte = input.value.trim();
+    if (texte === "") return;
+    
+    let tache = document.createElement("li");
+
+    let checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+
+    let span = document.createElement('span');
+    span.innerText = texte;
+
+    let supprimerBouton = document.createElement('button');
+    supprimerBouton.innerHTML = "Supprimer";
+
+    supprimerBouton.addEventListener('click', () => {
+        tache.remove();
+    });
+
+    checkbox.addEventListener('change', () => {
+    tache.classList.toggle('done', checkbox.checked);
+    
+
+  });
+
+    let taches = document.createElement('div');
+    tache.appendChild(checkbox);
+    tache.appendChild(span);
+    tache.appendChild(supprimerBouton);
+
+    liste.appendChild(tache);
+
+    input.value = "";
+});
